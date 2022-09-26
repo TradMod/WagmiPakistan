b5c613ae4ed080497b03ee16ee252aca8b58e37f
ABDul Rehman <devabdee@gmail.com>
2022-09-26 10:36:14 +0500

    programmatically verification added

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -2,12 +2,33 @@ const {ethers} = require("hardhat");
 
 async function main() {
   const wagmiPakistanContract = await ethers.getContractFactory("WagmiPakistan");
-
   const deployedWagmiPakistan = await wagmiPakistanContract.deploy();
-
   await deployedWagmiPakistan.deployed();
-
   console.log(`WagmiPakistan Contract Address: ${deployedWagmiPakistan.address}`);
+
+  // if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+    console.log("Waiting for block confirmations & Verifying...")
+    await deployedWagmiPakistan.deployTransaction.wait(5)
+    await verify(deployedWagmiPakistan.address, [])
+  // } else {
+  //   console.log("Verification Falied");
+  // }
+}
+
+const verify = async (contractAddress, args) => {
+  console.log("Verifying contract...")
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    })
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified!")
+    } else {
+      console.log(e)
+    }
+  }
 }
 
 main()
